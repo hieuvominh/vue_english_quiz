8aef5d237ddbb55cd4bea040dbbb28a21cd7599a
Hieu Vo <hieuvo@hubble.sg>
2019-08-04 20:48:13 +0700

    add file

---
+++ components/Articles.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="a-content">
+    <div v-for="(item, index) in items" :key="index">
+      <div>
+        {{item.question}}
+      </div>
+      <a-checkbox-group @change="onChange">
+        <a-row>
+          <a-col :span="24"><a-radio>(A)</a-radio><span>Hello</span></a-col>
+          <a-col :span="24"><a-radio>(B)</a-radio></a-col>
+          <a-col :span="24"><a-radio>(C)</a-radio></a-col>
+          <a-col :span="24"><a-radio>(D)</a-radio></a-col>
+        </a-row>
+      </a-checkbox-group>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: {
+    items: [
+      {question:'Hello', list: ['ABC','ABC','CDS','XXc']},
+      {question:'Hello', list: ['ABC','ABC','CDS','XXc']}
+    ]
+  },
+  methods: {
+    onChange (checkedValues) {
+      console.log('checked = ', checkedValues)
+    },
+  },
+}
+</script>
+<style>
+  .a-content{
+
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <Layout>
+    <Header><index /></Header>
+    <Layout>
+      <Content> <articles /></Content>
+    </Layout>
+  </Layout>
+</template>
+
+<script>
+import Index from '~/components/Index.vue'
+import Articles from '~/components/Articles.vue'
+import 'ant-design-vue/dist/antd.css'
+
+
+export default {
+  components: {
+    Index,
+    Articles
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
